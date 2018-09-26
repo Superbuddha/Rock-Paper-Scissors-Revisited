a3b21f5df9aa8946ee3f063fd2a522c609fa3d89
Stephan Constantial <Sconstantial@yahoo.com>
2018-09-26 19:22:59 +0100

    it's running

--- models/rps.rb
+++ models/rps.rb
@@ -1,15 +1,24 @@
-class Game
+class RPSGame
 
-  def self.check_win(player1, player2)
-    hand1 = player1[:hand].downcase
-    hand2 = player2[:hand].downcase
+  def self.check_win(hand1, hand2)
+    hand1 = hand1.downcase
+    hand2 = hand2.downcase
 
-    win = {"rock" => "scissors",
-    "scissor" => "paper",
-  "paper" => "rock"}
+    win = {
+      "rock" => "scissors",
+      "scissors" => "paper",
+      "paper" => "rock"
+    }
 
-  winner = nil
+    if hand1 == hand2
+      return "It's a Draw!"
+    elsif win[hand1] == hand2
+      return "Player 1 wins with #{hand1}!"
+    elsif win[hand2] == hand1
+      return "Player 2 wins with #{hand2}!"
+    else
+      return "uh oh! Looks like you didn't enter valid inputs!"
+    end
+  end
 
- if win[hand1] == hand2
-   winner = player1
- elsif win[hand2] == hand1
+end
